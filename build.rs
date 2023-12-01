90bc2392e026204e385db2943a97fef289cd861b
Xuejie Xiao <xxuejie@gmail.com>
2023-12-01 05:49:59 +0000

    Stack reorg sample

--- build.rs
+++ build.rs
@@ -12,7 +12,7 @@ fn main() {
         .include("deps/ckb-witness-args-handwritten-reader/c")
         .include("deps/ckb-c-stdlib")
         .include("deps/ckb-c-stdlib/libc")
-        .compiler(clang)
+        .compiler(clang.clone())
         .no_default_flags(true)
         .flag("--target=riscv64")
         .flag("-march=rv64imc_zba_zbb_zbc_zbs")
@@ -29,4 +29,14 @@ fn main() {
         .flag("-Wno-unused-parameter")
         .define("__SHARED_LIBRARY__", None)
         .compile("binding");
+
+    cc::Build::new()
+        .file("deps/ckb-stack-reorg-bootloader/bootloader.S")
+        .static_flag(true)
+        .compiler(clang)
+        .no_default_flags(true)
+        .flag("--target=riscv64")
+        .flag("-march=rv64imc_zba_zbb_zbc_zbs")
+        .flag("-O3")
+        .compile("bootloader");
 }
